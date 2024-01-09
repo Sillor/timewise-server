3c0e5b3020784d55dd43ba27776e8561c6b4333e
Eric <longeric464@gmail.com>
2024-01-09 13:38:13 -0800

    auth for registration function added.

--- API/UserAuthentication.cjs
+++ API/UserAuthentication.cjs
@@ -7,14 +7,56 @@ const app = express();
 
 require('dotenv').config();
 
-const port = process.env.PORT;
+const port = process.env.PORT1;
 
 app.use(cors());
 
 app.use(express.json());
 
-app.post(
-    "/register" , async function(req , res) {
-        
+//Create Account Call. Makes a new account and returns a jwt token
+app.post("/register",
+    async function (req, res) {
+        try {
+            //To do: Add duplicate check for email in database
+
+            //To do
+
+            const hashPW = await bcrypt.hash(req.body.password, 10)
+            const user = { "email": req.body.email, "password": hashPW }
+
+            //To do: Add SQL insert into database with hashed password
+
+            //To do
+
+            const accessToken = jwt.sign(user, process.env.JWT_KEY);
+
+            res.status(200).json({ "success": true, "token": accessToken })
+        } catch (error) {
+            console.log(error)
+            res.status(500).send("An error has occurred")
+        }
     }
 )
+
+//Login Account Call. Checks if a username and pass exist in database. If so, returns a jwt token
+app.post("/login",
+    async function (req, res) {
+        try {
+            //To do: Add SQL search for matching email, since email will be a unique identifier
+
+            //To do
+
+            const foundCombo = {"email" : "xxx" , "password" : "xxx"}
+
+            if (!bcrypt.compare(foundCombo.password , req.body.password)) {
+                res.status().send("Incorrect username or password.")
+            }
+
+        } catch (error) {
+            console.log(error)
+            res.status(500).send("An error has occurred")
+        }
+    }
+)
+
+app.listen(port, () => console.log(`Userdata Server listening on http://localhost:${port}`));
