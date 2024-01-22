d2d04165f1f6f1fa4208e842b3b671da23d4f0fe
Nolan Lawler <nlawler0207@gmail.com>
2024-01-22 14:39:39 -0800

    added CLIENT_PORT env variable

--- API/DatabaseManagement.cjs
+++ API/DatabaseManagement.cjs
@@ -42,7 +42,7 @@ app.use(express.json());
 app.use(cookieParser())
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: `http://localhost:${process.env.CLIENT_PORT}`,
   credentials: true,
 }));
 
